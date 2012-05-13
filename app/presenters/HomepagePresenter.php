ca9fd74c1b754647b5e7861ef91584439e1dabba
Vaclav Chalupa <vac.chalupa@gmail.com>
2012-05-13 16:16:07 +0200

    Oprava UTF-8 v sablone defaul.latte

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -11,7 +11,7 @@ class HomepagePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->tasks = $this->context->createTasks()->where(array('done' => false))->order('created ASC');
 	}
 
 }
